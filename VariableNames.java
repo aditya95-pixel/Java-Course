2f69a069741adb420db7e31f37f4c99a22cb3c6f
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-07-23 23:22:53 +0530

    added variable names file

---
+++ VariableNames.java
@@ -0,0 +1,12 @@
+public class VariableNames {
+    public static void main(String args[])
+    {
+        int num$haha_1=5;
+        int _x=6;
+        int $x=7;
+        System.out.println(num$haha_1);
+        System.out.println(_x);
+        System.out.println($x);
+    }
+    
+}
