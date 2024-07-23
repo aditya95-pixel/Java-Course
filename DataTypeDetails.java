b22acb212676a3fb70d5eb4617ed82039fcb5260
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-07-24 00:01:00 +0530

    Add files via upload

---
+++ DataTypeDetails.java
@@ -0,0 +1,8 @@
+public class DataTypeDetails {
+    public static void main(String args[])
+    {
+        int x1=5,x2=-5;
+        System.out.println(Integer.toBinaryString(x1));
+        System.out.println(Integer.toBinaryString(x2));
+    }
+}
