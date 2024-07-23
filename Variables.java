74b01252bb8d5d614b198cb09fc0d48459dee1cc
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-07-23 22:57:44 +0530

    added variables intro

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String args[])
+    {
+        byte b=5;
+        short s=100;
+        int i=300;
+        float a=25.3f;
+        char c='A';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
