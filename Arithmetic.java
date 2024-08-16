3d2bc22ce77bea93874cdc18eac3c36765552664
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-08-16 20:47:46 +0530

    arithmetic operators

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+public class Arithmetic{
+    public static void main(String[] args) {
+        int a=14,b=5;
+        int c=a/b;
+        float c1=(float)a/b;
+        int r=a%b;
+        System.out.println(c);
+        System.out.println(c1);
+        System.out.println(r);
+        float a1=14.3f,b1=3.2f;
+        float r1=a1%b1;
+        System.out.println(r1);
+    }
+}
