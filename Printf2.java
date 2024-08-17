1c0462a2bc9baa63148974e366637c266660a718
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-08-18 03:48:45 +0530

    Add files via upload

---
+++ Printf2.java
@@ -0,0 +1,18 @@
+public class Printf2 {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.printf("%05d\n",a);
+        System.out.printf("%(5d\n",a);
+        System.err.printf("%+5d\n",a);
+        a=-10;
+        System.out.printf("%(5d\n",a);
+        System.err.printf("%-5d\n",a);
+        float b=123.45f;
+        System.out.printf("%6.2f\n",b);
+        
+        String str="Java";
+        System.out.printf("%20s\n",str);
+        System.err.printf("%-20s\n",str);
+    }
+    
+}
