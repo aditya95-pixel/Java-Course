e44a9c03f6c1ed253634bf799de650318fe6235e
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-08-18 03:12:14 +0530

    println analysis

---
+++ Println.java
@@ -0,0 +1,7 @@
+public class Println {
+    public static void main(String args[]){
+        int x=10,y=20;
+        System.out.println("Sum:"+x+y);
+        System.out.println(x+y+"is the sum");
+    }
+}
