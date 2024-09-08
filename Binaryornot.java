a7709819f482e28d04077596abc5ce270e482b04
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-09-09 02:13:27 +0530

    Add files via upload

---
+++ Binaryornot.java
@@ -0,0 +1,11 @@
+public class Binaryornot {
+    public static void main(String[] args) {
+        int b=100110010;
+        
+        String str=String.valueOf(b);
+        
+        System.out.println(str.matches("[01]+"));
+        
+    }
+    
+}
