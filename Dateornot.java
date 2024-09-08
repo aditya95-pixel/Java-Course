5afceb33eb9b70b0e145780d86805990dded189d
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2024-09-09 02:23:07 +0530

    Add files via upload

---
+++ Dateornot.java
@@ -0,0 +1,6 @@
+public class Dateornot {
+    public static void main(String[] args) {
+        String d="01/12/2004";
+        System.out.println(d.matches("[0-3][0-9]/[0-1][0-9]/[0-9]{4}"));
+    }
+}
---
+++ Hexaornot.java
@@ -0,0 +1,6 @@
+public class Hexaornot {
+    public static void main(String[] args) {
+        String h="23AD";
+        System.out.println(h.matches("[0-9A-Z]+"));
+    }
+}
